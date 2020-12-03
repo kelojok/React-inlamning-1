64f974ded140c4a7df04441d09b1c43982959ab6
kelojok <55785050+kelojok@users.noreply.github.com>
2020-12-03 10:06:17 +0100

    Username and pw functionality with useState
    Added user and pw fields that works with useState

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginView = () => {
 
-    return ( <div>This is the login view</div>
+    // First value is a reference and the 2nd one is a function that updates the state
+    const[username, setUsername] = useState()
+    const [password, setPassword] = useState()
+
+    return ( <div>
+    <span>Username: </span><input onChange={event => setUsername(event.target.value)} />
+    <br />
+    <span>Password: </span><input type="password" onChange={event => setPassword(event.target.value)} />
+    <button></button>
+
+    </div>
 
     )
 }
