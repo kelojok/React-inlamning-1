e2e82c779c53f5fe000b8b3a1a1c8ce317be51e7
kelojok <55785050+kelojok@users.noreply.github.com>
2020-12-07 11:39:18 +0100

    Added useEffect to MainView(inl upg 2)
    inl upg 2: useEffect added to the MainView

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const MainView = () => {
 
-    return ( <div>This is the main view</div>
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        // Render at start
+        alert("using useEffect in the beginning");
+
+        // Render at end/switching component
+        return () => {
+            alert("using useEffect in the end")
+        }
+    }, [count])
+
+    return ( <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increment with 1</button>
+    </div>
 
     )
 }
